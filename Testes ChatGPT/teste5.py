feae7b8a1b55cd338489f74814792b1210e86e96
IsaacYusuke <129394732+IsaacYusuke@users.noreply.github.com>
2025-02-04 18:08:45 -0300

    PyTorch funcionou

--- Testes ChatGPT/teste5.py
+++ Testes ChatGPT/teste5.py
@@ -1,13 +1,19 @@
-#Gerador de texto - transformers - usando tensorflow - DIGITA TEXTO EM INGLÊS
+#Gerador de texto - transformers - usando tensorflow e PyTorch - DIGITA TEXTO EM INGLÊS
 
 from transformers import pipeline
 
 # Carregar o modelo pré-treinado GPT-2
-modelo = pipeline("text-generation", model="facebook/opt-350m")
+modelo = pipeline("text-generation", model="facebook/opt-1.3b", framework="pt") #Instalei PyTorch
 
 # Função para gerar texto
 def gerar_texto(prompt, max_palavras=100):
-    resultado = modelo(prompt, max_length=max_palavras, num_return_sequences=1) #, temperature=0.7, top_k=50, top_p=0.9)
+    resultado = modelo(prompt, 
+                   max_length=max_palavras, 
+                   temperature=0.7,  # Reduz aleatoriedade
+                   top_k=50, 
+                   top_p=0.9, 
+                   num_return_sequences=1
+                  ) 
     return resultado[0]['generated_text']
 
 # Testar o gerador de texto
